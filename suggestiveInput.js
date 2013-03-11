5c8091c528b4fc145f1b3963308ee4ceabcfbb05
Michael Joy <mjoy@joyit.ie>
2013-03-11 02:38:08 +0000

    Test commit
    this commit my first git commit just to test it!

---
+++ suggestiveInput.js
@@ -0,0 +1,7 @@
+/********* Suggestive javascript input box **********************/
+/********* Written by Michael Joy for free open source use ******/
+
+//Holds the number of times suggestion boxes have been printed...
+
+
+function suggestiveInput(name)
